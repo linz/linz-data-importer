75d2de1d759179bc8bf6772432c2101681678a27
splanzer <splanzer@linz.govt>
2018-09-26 15:52:46 +1200

    test

--- linz-data-importer/tests/run_tests.py
+++ linz-data-importer/tests/run_tests.py
@@ -28,3 +28,5 @@ def run_test_modules():
     """
     test_suite = unittest.TestLoader().discover(__location__, pattern="test_*.py")
     unittest.TextTestRunner(verbosity=3, stream=sys.stdout).run(test_suite)
+
+run_test_modules()
