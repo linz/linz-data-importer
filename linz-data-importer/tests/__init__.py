d429d3d79c13e7a94984539c9b85c62e3b646914
splanzer <splanzer@linz.govt>
2018-09-28 11:22:26 +1200

    test

--- linz-data-importer/tests/__init__.py
+++ linz-data-importer/tests/__init__.py
@@ -6,5 +6,5 @@ from test_ldi_plugin import UnitLevel
 UiTest
 def run_tests():
     suite = unittest.TestSuite()
-    suite.addTests(unittest.makeSuite(UnitLevel, "test"))
+    suite.addTest(unittest.makeSuite(UnitLevel))
     unittest.TextTestRunner(verbosity=2, stream=sys.stdout).run(suite)
