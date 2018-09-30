6b8cf6915cb3abad2d3683e71fcd04ec2eca1fdc
splanzer <splanzer@linz.govt>
2018-10-01 11:39:42 +1300

    travis test

--- linz-data-importer/tests/__init__.py
+++ linz-data-importer/tests/__init__.py
@@ -1,12 +1,19 @@
 import sys
 import unittest
+import os
 
-#from test_ldi_plugin import UnitLevel
-from test_ldi_ui_elements import  UiTest
-from test_ldi_integration import CorruptXml, UserWorkFlows
-
+# from test_ldi_plugin import UnitLevel
+# from test_ldi_ui_elements import  UiTest
+# from test_ldi_integration import CorruptXml, UserWorkFlows
+# 
+# def run_tests():
+#     suite = unittest.TestSuite()
+#     test_suite = unittest.TestLoader().discover(__location__, pattern="test_*.py")
+# 
+#     #suite.addTest(unittest.makeSuite(UnitLevel))
+#     suite.addTest(unittest.makeSuite(UserWorkFlows))
+#     unittest.TextTestRunner(verbosity=2, stream=sys.stdout).run(suite)
 def run_tests():
-    suite = unittest.TestSuite()
-    #suite.addTest(unittest.makeSuite(UnitLevel))
-    suite.addTest(unittest.makeSuite(UserWorkFlows))
-    unittest.TextTestRunner(verbosity=2, stream=sys.stdout).run(suite)
+
+    test_suite = unittest.TestLoader().discover(__location__, pattern="test_*.py")
+    unittest.TextTestRunner(verbosity=2, stream=sys.stdout).run(test_suite)
