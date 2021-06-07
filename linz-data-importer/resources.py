e764c5ca3b4142e392739d291dd2d886e03932fc
Bill Nelson <bmnelson@linz.govt.nz>
2021-06-07 20:51:09 +1200

    ci: disable pylint invalid-name

--- linz-data-importer/resources.py
+++ linz-data-importer/resources.py
@@ -8,6 +8,8 @@
 
 from PyQt5 import QtCore
 
+# pylint:disable=invalid-name
+
 qt_resource_data = b"\
 \x00\x00\x04\x59\
 \x89\
@@ -806,7 +808,7 @@ qt_resource_struct_v2 = b"\
 \x00\x00\x01\x75\xb5\x54\x70\x0b\
 "
 
-qt_version = [int(v) for v in QtCore.qVersion().split('.')]
+qt_version = [int(v) for v in QtCore.qVersion().split(".")]
 if qt_version < [5, 8, 0]:
     rcc_version = 1
     qt_resource_struct = qt_resource_struct_v1
@@ -814,10 +816,17 @@ else:
     rcc_version = 2
     qt_resource_struct = qt_resource_struct_v2
 
+
 def qInitResources():
-    QtCore.qRegisterResourceData(rcc_version, qt_resource_struct, qt_resource_name, qt_resource_data)
+    QtCore.qRegisterResourceData(
+        rcc_version, qt_resource_struct, qt_resource_name, qt_resource_data
+    )
+
 
 def qCleanupResources():
-    QtCore.qUnregisterResourceData(rcc_version, qt_resource_struct, qt_resource_name, qt_resource_data)
+    QtCore.qUnregisterResourceData(
+        rcc_version, qt_resource_struct, qt_resource_name, qt_resource_data
+    )
+
 
 qInitResources()
